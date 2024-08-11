971169d52b78acbeb406c2bc81214b9de3396bd2
Miha Markič <miha@rthand.com>
2024-08-11 20:46:38 +0200

    Update scoop helper scripts

--- src/C64AssemblerStudio/Scripts/create_startmenu_shortcut.ps1
+++ src/C64AssemblerStudio/Scripts/create_startmenu_shortcut.ps1
@@ -1,4 +1,16 @@
-﻿$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\ColorPix.lnk")
-$Shortcut.TargetPath = "C:\Program Files (x86)\ColorPix\ColorPix.exe"
+﻿# Creates Start Menu entry
+$ProgramsPath = [Environment]::GetFolderPath([System.Environment+SpecialFolder]::Programs)
+$ShortcutDirectory = [IO.Path]::Combine($ProgramsPath, "C64AssemblerStudio")
+Write-Host $ShortcutDirectory
+if (-Not (Test-Path -Path $ShortcutDirectory)) {
+	[IO.Directory]::CreateDirectory($ShortcutDirectory)
+	Write-Host("Directory created")
+} else {
+	Write-Host("Directory already exists")
+}
+$WshShell = New-Object -comObject WScript.Shell
+$AppLink = [IO.Path]::Combine($ShortcutDirectory, "C64 Assembler Studio.lnk")
+$Shortcut = $WshShell::CreateShortcut($AppLink)
+$Shortcut.TargetPath = [IO.Path]::Combine($PSScriptRoot, "C64AssemblerStudio.exe")
+$Shortcut.Description = "An IDE for building C64 assembler programs using VICE emulator"
 $Shortcut.Save()
---
+++ src/C64AssemblerStudio/Scripts/delete_startmenu_shortcut.ps1
@@ -0,0 +1,10 @@
+﻿# Creates Start Menu entry
+$ProgramsPath = [Environment]::GetFolderPath([System.Environment+SpecialFolder]::Programs)
+$ShortcutDirectory = [IO.Path]::Combine($ProgramsPath, "C64AssemblerStudio")
+Write-Host $ShortcutDirectory
+if (Test-Path -Path $ShortcutDirectory) {
+	[IO.Directory]::Delete($ShortcutDirectory, $true)
+	Write-Host("Directory deleted")
+} else {
+	Write-Host("Directory does not exist")
+}
