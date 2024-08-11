3bb6a6cb8a390f3ee196801b98ffb038ac9d0684
Miha Markič <miha@rthand.com>
2024-08-11 20:50:38 +0200

    Adds builder tasks

---
+++ src/C64AssemblerStudioBuilder/Builder/Publish/PublishTask.cs
@@ -0,0 +1,38 @@
+﻿using System.IO.Compression;
+using System.Xml.Linq;
+using Cake.Common.Diagnostics;
+using Cake.Common.IO;
+using Cake.Frosting;
+
+namespace Build.Publish;
+
+[TaskName("Publish")]
+[IsDependentOn(typeof(TestTask))]
+[IsDependentOn(typeof(ScoopCompressPublishTask))]
+public class PublishTask: FrostingTask
+{
+    
+}
+
+[TaskName("ScoopCompressPublish")]
+[IsDependentOn(typeof(ScoopPublishTask))]
+public class ScoopCompressPublishTask : FrostingTask<BuildContext>
+{
+    public override bool ShouldRun(BuildContext context) => context.BuildType == BuildType.Scoop;
+    public override void Run(BuildContext context)
+    {
+        var csprojectXml = XDocument.Load(context.MakeAbsolute(context.DesktopProject).FullPath);
+        var versionText = csprojectXml.Root!.Element("PropertyGroup")!.Element("Version")!.Value;
+        var source = context.MakeAbsolute(context.PublishDirectory).FullPath;
+        string version = versionText.Split('-')[0].Replace('.', '_');
+        string fileName = $"C64AssemblerStudio_{version}_{context.ArchitectureUnderscoreText}.zip";
+        context.Information($"Compressed file is {fileName}");
+        var target = context.MakeAbsolute(context.PublishRootDirectory.CombineWithFilePath(context.File(fileName))).FullPath;
+
+        if (context.FileExists(target))
+        {
+            context.DeleteFile(target);
+        }
+        ZipFile.CreateFromDirectory(source, target);
+    }
+}
---
+++ src/C64AssemblerStudioBuilder/Builder/Publish/ScoopPublishTask.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Cake.Common.Diagnostics;
+using Cake.Common.IO;
+using Cake.Common.Tools.DotNet;
+using Cake.Common.Tools.DotNet.Publish;
+using Cake.Common.Tools.SignTool;
+using Cake.Core.IO;
+using Cake.Frosting;
+
+namespace Build.Publish;
+
+[TaskName("ScoopPublish")]
+[IsDependentOn(typeof(SignWinX64Task))]
+public class ScoopPublishTask : FrostingTask<BuildContext>
+{
+    public override bool ShouldRun(BuildContext context) => context.BuildType == BuildType.Scoop;
+}
+
+[TaskName("ScoopPublishWinX64")]
+public class ScoopPublishWinX64Task : FrostingTask<BuildContext>
+{
+    public override bool ShouldRun(BuildContext context) =>
+        context.BuildType == BuildType.Scoop && context.Architecture == TargetArchitecture.WinX64;
+
+    public override void Run(BuildContext context)
+    {
+        Publish.Scoop(context);
+        var fileName = context.File("*_startmenu_shortcut.ps1");
+        GlobPattern sourceGlob = context.MakeAbsolute(context.SolutionScriptsDirectory.CombineWithFilePath(fileName)).FullPath;
+
+        context.CopyFiles(sourceGlob, context.PublishDirectory);
+        context.Information($"Added script files");
+    }
+}
+
+[TaskName("SignWin")]
+[IsDependentOn(typeof(ScoopPublishWinX64Task))]
+public class SignWinX64Task : FrostingTask<BuildContext>
+{
+    public override bool ShouldRun(BuildContext context) =>
+        context.BuildType == BuildType.Scoop && context.Architecture == TargetArchitecture.WinX64;
+
+    public override void Run(BuildContext context)
+    {
+        var settings = new SignToolSignSettings
+        {
+            TimeStampUri = new Uri("http://timestamp.digicert.com"),
+            CertThumbprint = "df28fe69c52288eeff676fec3f6167e40c0564f8",
+            DigestAlgorithm = SignToolDigestAlgorithm.Sha256,
+        };
+        
+        var exeFile = context.PublishDirectory.CombineWithFilePath("C64AssemblerStudio.exe");
+        context.Information($"File to sign is {exeFile}");
+        try
+        {
+            context.Sign(exeFile, settings);
+        }
+        catch (Exception e)
+        {
+            context.Error($"Signtool should be in %PATH%. To get its location one can use '$(get-command signtool).path' powershell command.");
+            throw;
+        }
+    }
+}
+
+static class Publish
+{
+    internal static void Scoop(BuildContext context)
+    {
+        var settings = new DotNetPublishSettings
+        {
+            Configuration = "Release",
+            SelfContained = true,
+            Runtime = context.TargetRuntime,
+            OutputDirectory = context.PublishDirectory,
+        };
+        
+        context.DeleteDirectory(context.PublishDirectory, new DeleteDirectorySettings{ Force = true, Recursive = true });
+        context.DotNetPublish(context.DesktopProject.FullPath, settings);
+    }
+}
