9be320bed61e445c8b4e4b9d168893c0c4d35629
Miha Markič <miha@rthand.com>
2024-09-19 16:08:12 +0200

    Adds PC register to breakpoint conditions

--- src/C64AssemblerStudio/C64AssemblerStudio.Engine/C64Globals.cs
+++ src/C64AssemblerStudio/C64AssemblerStudio.Engine/C64Globals.cs
@@ -7,5 +7,5 @@ public static class C64Globals
     public static FrozenSet<string> MemspacePrefixes { get; } =
         FrozenSet.ToFrozenSet(["c", "8", "9", "10", "11"], StringComparer.OrdinalIgnoreCase);
     public static FrozenSet<string> Registers { get; } =
-        FrozenSet.ToFrozenSet(["A", "X", "Y", "SP"], StringComparer.OrdinalIgnoreCase);
+        FrozenSet.ToFrozenSet(["A", "X", "Y", "SP", "PC"], StringComparer.OrdinalIgnoreCase);
 }
