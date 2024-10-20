99af79265e7b2baec57314d39cb424a7a9d8ed80
Miha Markič <miha@rthand.com>
2024-10-20 10:33:38 +0200

    Implements project change detection in project explorer

--- src/C64AssemblerStudio/C64AssemblerStudio.Engine/Services/Abstract/IProjectFileWatcher.cs
+++ src/C64AssemblerStudio/C64AssemblerStudio.Engine/Services/Abstract/IProjectFileWatcher.cs
@@ -2,5 +2,4 @@
 
 public interface IProjectFileWatcher : IDisposable, IAsyncDisposable
 {
-    
 }
--- src/C64AssemblerStudio/C64AssemblerStudio.Engine/ViewModels/ProjectFilesWatcherViewModel.cs
+++ src/C64AssemblerStudio/C64AssemblerStudio.Engine/ViewModels/ProjectFilesWatcherViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Frozen;
+using System.ComponentModel;
 using System.Diagnostics;
+using C64AssemblerStudio.Core;
 using C64AssemblerStudio.Engine.Messages;
 using C64AssemblerStudio.Engine.Models.Projects;
 using C64AssemblerStudio.Engine.Services.Abstract;
@@ -14,7 +16,10 @@ public class ProjectFilesWatcherViewModel: ViewModel
 {
     private readonly ILogger<ProjectFilesWatcherViewModel> _logger;
     private readonly IServiceFactory _serviceFactory;
-    private readonly List<IProjectFileWatcher> _projectFileWatchers = new();
+    /// <summary>
+    /// Keeps tracks of file watchers.
+    /// </summary>
+    private readonly Dictionary<ProjectDirectory, IProjectFileWatcher> _projectFileWatchers = new();
     private readonly Globals _globals;
     public bool IsRefreshing { get; private set; }
     public bool IsProjectChanging { get; private set; }
@@ -22,8 +27,7 @@ public class ProjectFilesWatcherViewModel: ViewModel
     public ObservableCollection<ProjectItem> Items => Root?.Items ?? [];
     public ProjectLibraries? Libraries { get; private set; }
     public ProjectRoot? Root { get; private set; }
-
-    ISubscription _projectChangedSubscription;
+    private readonly ISubscription _projectChangedSubscription;
     public ProjectFilesWatcherViewModel(ILogger<ProjectFilesWatcherViewModel> logger, IServiceFactory serviceFactory,
         Globals globals, IDispatcher dispatcher)
     {
@@ -46,7 +50,47 @@ public class ProjectFilesWatcherViewModel: ViewModel
 
     private async Task ProjectChanged(ProjectChangedMessage message, CancellationToken ct)
     {
-        
+        await SyncLibrariesWithProjectAsync(ct);
+    }
+
+    private async Task SyncLibrariesWithProjectAsync(CancellationToken ct)
+    {
+        if (Libraries is not null)
+        {
+            var libraries = Libraries.Items.ToFrozenDictionary(l => l.AbsolutePath, OsDependent.FileStringComparer);
+            var newLibraries = _globals.Project.Libraries.ToHashSet(OsDependent.FileStringComparer);
+            // first remove old ones
+            foreach (var l in libraries)
+            {
+                if (!newLibraries.Contains(l.Key))
+                {
+                    Libraries.Items.Remove(l.Value);
+                    _logger.LogInformation("Removing library node {AbsolutePath}", l.Key);
+                }
+                else
+                {
+                    newLibraries.Remove(l.Key);
+                }
+            }
+
+            if (newLibraries.Count > 0)
+            {
+                // adds remaining new libraries
+                await AddNewLibrariesAsync(newLibraries, ct);
+                _logger.LogInformation("Adding these new libraries {Libraries}", string.Join(", ", newLibraries));
+            }
+        }
+        else
+        {
+            _logger.LogWarning("Project changed when Libraries node is not set");
+        }
+    }
+
+    private async Task AddNewLibrariesAsync(ISet<string> libraries, CancellationToken ct)
+    {
+        var directory = Path.GetDirectoryName(_globals.Project.Path).ValueOrThrow();
+        var tasks = libraries.Select(l => AddLibraryAsync(directory, l, ct));
+        await Task.WhenAll(tasks);
     }
 
     private CancellationTokenSource? _projectChangingCts;
@@ -63,7 +107,7 @@ public class ProjectFilesWatcherViewModel: ViewModel
             await RefreshAsync();
             if (project.Directory is not null && !token.IsCancellationRequested)
             {
-                Start(project.Directory, project.Libraries);
+                EnableDirectoryWatchers(project.Directory, project.Libraries);
             }
         }
         finally
@@ -95,7 +139,7 @@ public class ProjectFilesWatcherViewModel: ViewModel
             if (IsProjectOpen)
             {
                 _refreshCts = new CancellationTokenSource();
-                var ct = _refreshCts.Token;
+                var ctInternal = _refreshCts.Token;
 
                 var project = _globals.Project;
                 var directory = Path.GetDirectoryName(project.Path).ValueOrThrow();
@@ -109,20 +153,11 @@ public class ProjectFilesWatcherViewModel: ViewModel
                 Libraries = new ProjectLibraries { Parent = null, Name = "Libraries" }; 
                 Root.Items.Add(Libraries);
                 var tasks = new List<Task>(project.Libraries.Length + 1);
-                var projectRefreshTask = RefreshProjectStructureAsync(Root, ct);
+                var projectRefreshTask = RefreshProjectStructureAsync(Root, ctInternal);
                 tasks.Add(projectRefreshTask);
-                for (int i = 0; i < project.Libraries.Length; i++)
+                foreach (var libraryPath in project.Libraries)
                 {
-                    var libraryPath = project.Libraries[i];
-                    string absoluteLibraryPath = Path.GetFullPath(Path.Combine(directory, libraryPath));
-                    var library = new ProjectLibrary
-                    {
-                        Name = Path.GetFileNameWithoutExtension(libraryPath),
-                        Parent = null,
-                        AbsoluteRootPath = absoluteLibraryPath,
-                    };
-                    Libraries.Items.Add(library);
-                    tasks.Add(RefreshProjectStructureAsync(library, ct));
+                    tasks.Add(AddLibraryAsync(directory, libraryPath, ctInternal));
                 }
 
                 await Task.WhenAll(tasks);
@@ -132,6 +167,19 @@ public class ProjectFilesWatcherViewModel: ViewModel
         {
             IsRefreshing = false;
         }
+    }
+
+    private async Task AddLibraryAsync(string projectDirectory, string relativePath, CancellationToken ct)
+    {
+        string absoluteLibraryPath = Path.GetFullPath(Path.Combine(projectDirectory, relativePath));
+        var library = new ProjectLibrary
+        {
+            Name = Path.GetFileNameWithoutExtension(relativePath),
+            Parent = null,
+            AbsoluteRootPath = absoluteLibraryPath,
+        };
+        Libraries.ValueOrThrow().Items.Add(library);
+        await RefreshProjectStructureAsync(library, ct);
     }
 
     private async Task RefreshProjectStructureAsync(ProjectRootDirectory rootDirectory, CancellationToken ct)
@@ -163,7 +211,7 @@ public class ProjectFilesWatcherViewModel: ViewModel
             }
             else
             {
-                _logger.LogError("Project directory {Directory} does not exist", rootDirectory.AbsolutePath);
+                _logger.LogError("Directory {Directory} does not exist", rootDirectory.AbsolutePath);
             }
         }
         catch (Exception ex)
@@ -172,25 +220,26 @@ public class ProjectFilesWatcherViewModel: ViewModel
         }
     }
     
-    void Start(string rootDirectory, ImmutableArray<string> libraries)
+    void EnableDirectoryWatchers(string rootDirectory, ImmutableArray<string> libraries)
     {
-        var fileWatcher = _serviceFactory.CreateProjectFileWatcher(Root.ValueOrThrow());
-        _projectFileWatchers.Add(fileWatcher);
-        for (int i = 0; i < libraries.Length; i++)
+        AddFileWatcher(Root.ValueOrThrow());
+        foreach (var library in Libraries.ValueOrThrow().Items)
         {
-            var libraryPath = libraries[i];
-            var library = Libraries.Items[i];
-            var absoluteLibraryDirectory = Path.Combine(rootDirectory, libraryPath);
-            var libraryFileWatcher =  _serviceFactory.CreateProjectFileWatcher(library);
-            _projectFileWatchers.Add(libraryFileWatcher);
+            AddFileWatcher(library);
         }
+    }
+
+    private void AddFileWatcher(ProjectRootDirectory rootDirectory)
+    {
+        var fileWatcher =  _serviceFactory.CreateProjectFileWatcher(rootDirectory);
+        _projectFileWatchers.Add(rootDirectory, fileWatcher);
     }
 
     void Disengage()
     {
         foreach (var fileWatcher in _projectFileWatchers)
         {
-            fileWatcher.Dispose();
+            fileWatcher.Value.Dispose();
         }
         _projectFileWatchers.Clear();
     }
@@ -218,6 +267,7 @@ public class ProjectFilesWatcherViewModel: ViewModel
     {
         if (!IsDisposed && disposing)
         {
+            _projectChangedSubscription.Dispose();
             _globals.PropertyChanged -= GlobalsOnPropertyChanged;
         }
         base.Dispose(disposing);
