156677a849d85741a345f086a9b21fc625b17ac9
Miha Markič <miha@rthand.com>
2024-11-18 17:22:07 +0100

    Fixes ReferencedFileElementGenerator bug when user deletes text

--- src/C64AssemblerStudio/C64AssemblerStudio.Desktop/Controls/SyntaxEditor/ReferencedFileElementGenerator.cs
+++ src/C64AssemblerStudio/C64AssemblerStudio.Desktop/Controls/SyntaxEditor/ReferencedFileElementGenerator.cs
@@ -29,17 +29,31 @@ namespace C64AssemblerStudio.Desktop.Controls.SyntaxEditor
 
         public override int GetFirstInterestedOffset(int startOffset)
         {
-            Debug.WriteLine($"{nameof(GetFirstInterestedOffset)} on {startOffset}");
+            // Debug.WriteLine($"{nameof(GetFirstInterestedOffset)} on {startOffset}");
             if (_maxOffset >= startOffset)
             {
                 foreach (var rf in _referencedFiles)
                 {
                     if (rf.Start >= startOffset)
                     {
-                        return rf.Start;
+                        var endOffset = CurrentContext.VisualLine.LastDocumentLine.EndOffset;
+                        int maxLength = endOffset - rf.Start;
+                        if (maxLength >= rf.Length)
+                        {
+                            var text = CurrentContext.GetText(rf.Start + 1,
+                                rf.ReferencedFile.RelativeFilePath.Length);
+                            var match = text.Text.AsSpan()[text.Offset..(text.Offset + text.Count)];
+                            // Debug.WriteLine(
+                            //     $"Matched {match} on offset {rf.Start} against {rf.ReferencedFile.RelativeFilePath}");
+                            if (match.SequenceEqual(rf.ReferencedFile.RelativeFilePath))
+                            {
+                                return rf.Start;
+                            }
+                        }
                     }
                 }
             }
+
             return -1;
         }
 
@@ -47,6 +61,8 @@ namespace C64AssemblerStudio.Desktop.Controls.SyntaxEditor
         {
             Debug.WriteLine($"Generating visual element at {offset}");
             var rf = _referencedFilesMap[offset];
+            Debug.WriteLine($"Creating {rf.Length} from offset {offset} on line length {CurrentContext.VisualLine.VisualLength}");
+            var line = CurrentContext.VisualLine;
             return new ReferencedFileVisualLineLinkText(CurrentContext.VisualLine, rf.Length, rf, OnClicked);
         }
     }
